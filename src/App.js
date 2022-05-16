6b40a2cb6775a45d84eadc4eef894c283e5c65d4
Rodrigo - Xita rps <xitarps@gmail.com>
2022-05-15 23:37:09 -0300

    [Feature]: Set production ready url for fetch

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Notes from './components/Notes';
 import { useEffect, useState } from 'react';
 
 function App() {
-  const URL = 'http://127.0.0.1:3000/api/v1/notes';
+  // const URL = 'http://127.0.0.1:3000/api/v1/notes';
+  const URL = 'https://to-do-list-back-ruby-on-rails.herokuapp.com/api/v1/notes';
 
   const [notes, setNotes] = useState([])
 
