5829242912b46899f20c01071828f0a158b9c621
Rodrigo - Xita rps <xitarps@gmail.com>
2022-05-16 00:02:52 -0300

    [Featrure]: Add fetch POST to back-end to create a note

--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import './NoteForm.css';
 
 const NoteForm = () => {
+
+  const [state, setState] = useState({});
+
+  const handleChange = (event) => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  const handleSubmit = (event) => {
+    //const URL = 'http://127.0.0.1:3000/api/v1/notes';
+    const URL = 'https://to-do-list-back-ruby-on-rails.herokuapp.com/api/v1/notes';
+
+    return(
+      fetch(URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ note: state })
+      }).then(response => response.json())
+    )
+  };
+
+
   return (
     <div>
       <h2>Add a note</h2>
@@ -11,26 +37,26 @@ const NoteForm = () => {
             <span>
               Title:
             </span>
-            <input type="text" name="title" />
+            <input type="text" name="title" onChange={handleChange}/>
           </label>
         </p>
 
         <p>
           <label className="form-input-and-label">
             Description:
-            <textarea name="description" id="txtArea" rows="5" cols="20"></textarea>
+            <textarea name="description" id="txtArea" rows="5" cols="20" onChange={handleChange}></textarea>
           </label>
         </p>
 
         <p>
           <label className="form-input-and-label">
             Resolving Date:
-            <input type="datetime-local" name="resolving_date" />
+            <input type="datetime-local" name="resolving_date" onChange={handleChange} />
           </label>
         </p>
 
         <p>
-          <button type="submit" value="Submit"> Salvar</button>
+          <button type="submit" value="Submit" onClick={handleSubmit}> Salvar</button>
         </p>
       </div>
     </div>
