2fa29274dc75f0824bdb8f9f6853335690c75b2e
Rodrigo - Xita rps <xitarps@gmail.com>
2022-05-15 23:24:33 -0300

    [Feature]: Add fetch notes from back-end and basic style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Notes from './components/Notes';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const URL = 'http://127.0.0.1:3000/api/v1/notes';
+
+  const [notes, setNotes] = useState([])
+
+  const getNotes = () => {
+    return(
+      fetch(URL, {
+        methods: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(response => response.json())
+    )
+  }
+
+  useEffect(() => {
+    getNotes().then(data => setNotes(data))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>To-do List</h1>
       </header>
       <main className="App-main">
-        <Notes />
+        <Notes notes={notes}/>
       </main>
     </div>
   );
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,9 +1,21 @@
 import react from 'react';
+import './Notes.css';
 
-const Notes = () => {
+const Notes = (props) => {
   return(
     <div>
-      To do data...
+      <h2>Notes</h2>
+      {
+        props.notes.map(note => {
+          return(
+            <div key={note.id}>
+              <h3 className="note-title">{note.title}</h3>
+              <p>{note.description}</p>
+              <p><strong>{note.resolving_date}</strong></p>
+            </div>
+          )
+        })
+      }
     </div>
   )
 };
