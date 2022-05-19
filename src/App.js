6a40adc62a493679b9a57f04ba8d70e4d9114898
Rodrigo - Xita rps <xitarps@gmail.com>
2022-05-18 21:38:36 -0300

    [Wip]: Add Update feature

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Notes from './components/Notes';
 import NoteForm from './components/NoteForm';
 
 function App() {
-  // const URL = 'http://127.0.0.1:3000/api/v1/notes';
-  const URL = 'https://to-do-list-back-ruby-on-rails.herokuapp.com/api/v1/notes';
+  const URL = 'http://127.0.0.1:3000/api/v1/notes';
+  // const URL = 'https://to-do-list-back-ruby-on-rails.herokuapp.com/api/v1/notes';
 
   const [notes, setNotes] = useState([])
 
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -4,8 +4,8 @@ import './Notes.css';
 const Notes = (props) => {
 
   const deleteNote = (id) => {
-    // const URL = 'http://127.0.0.1:3000/api/v1/notes/' + id;
-    const URL = 'https://to-do-list-back-ruby-on-rails.herokuapp.com/api/v1/notes/' + id;
+    const URL = 'http://127.0.0.1:3000/api/v1/notes/' + id;
+    // const URL = 'https://to-do-list-back-ruby-on-rails.herokuapp.com/api/v1/notes/' + id;
 
     fetch(URL, {
       method: 'DELETE',
@@ -21,6 +21,31 @@ const Notes = (props) => {
     )
   }
 
+  // const editNote = (id) => {
+  //   const URL = 'http://127.0.0.1:3000/api/v1/notes/' + id;
+  //   // const URL = 'https://to-do-list-back-ruby-on-rails.herokuapp.com/api/v1/notes/' + id;
+
+  //   fetch(URL, {
+  //     method: 'PATCH',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify({ note: props.notes.find(note => note.id === id) }) TODO <=======================
+  //   }).then(response => response.json())
+  //     .then(data => {
+  //       props.setNotes(
+  //         props.notes.map(note => {
+  //           if (note.id === data.id) {
+  //             return data
+  //           } else {
+  //             return note
+  //           }
+  //         })
+  //       );
+  //     }
+  //   )
+  // }
+
   return(
     <>
       <h2>Notes</h2>
@@ -29,7 +54,9 @@ const Notes = (props) => {
           props.notes.map(note => {
             return(
               <div key={note.id}>
-                <h3 className="note-title">{note.title} <span className='close-cross' onClick={() => deleteNote(note.id)} >✖</span></h3>
+                {/* <span className='close-cross' onClick={() => editNote(note.id)} >✑</span> |  */}
+                <span className='close-cross' onClick={() => deleteNote(note.id)} >✖</span>
+                <h3 className="note-title">{note.title} </h3>
                 <p>{note.description}</p>
                 <p><strong>{new Date(note.resolving_date).toLocaleString("pt-BR", {timeZone: "UTC"})}</strong></p>
               </div>
